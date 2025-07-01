a9757ad9f1c3606b511622e976382f5d251f4351
ClintonBeyene <clintonbeyene52@gmail.com>
2025-07-01 23:52:03 +0300

    Adding and Checking Out Books

--- Entry-Level/Guided - Build a Library System with Python/library.py
+++ Entry-Level/Guided - Build a Library System with Python/library.py
@@ -3,3 +3,13 @@ class Library:
     def __init__(self):
         self.inventory = []
     
+    def add_book(self, book):
+        self.inventory.append(book)
+    
+    def checkout_book(self, book, borrower):
+        if book.available:
+            book.available = False
+            borrower.borrowed_books.append(book)
+        else:
+            print(book.title + " is not available.")
+    
